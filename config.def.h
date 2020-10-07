652ce5a4d26dde3c83f18a656006eb4a86b9214d
dragondove <jinxiongrong@foxmail.com>
2020-10-07 16:34:45 +0800

    Fix compile error for low version of gcc

--- config.def.h
+++ config.def.h
@@ -13,8 +13,10 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#888888";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#444444";
-static const unsigned int baralpha = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
+#define baralpha 0xd0
+#define borderalpha OPAQUE
+// static const unsigned int baralpha = 0xd0;
+// static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
